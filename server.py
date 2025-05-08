ab106f3646f43896775de2522ed164800da07d12
13797073323 <137034656@qq.com>
2025-05-08 17:30:14 +0800

    put

--- server.py
+++ server.py
@@ -7,10 +7,21 @@ class TupleSpace:
     def __init__(self):
         self.store = dict()
         self.lock = threading.Lock()
-        
+
         self.total_clients = 0
         self.total_ops = 0
         self.read_count = 0
         self.get_count = 0
         self.put_count = 0
         self.error_count = 0
+
+    def put(self, key, value):
+        with self.lock:
+            if key in self.store:
+                self.error_count += 1
+                return "ERR", f"{key} already exists"
+            else:
+                self.store[key] = value
+                self.put_count += 1
+                self.total_ops += 1
+                return "OK", f"({key}, {value}) added"
