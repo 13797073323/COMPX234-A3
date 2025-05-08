4c274ef655fd43cc4eb83d5c6081d29da13b3876
13797073323 <137034656@qq.com>
2025-05-08 15:50:35 +0800

    send_request

--- client.py
+++ client.py
@@ -2,3 +2,14 @@ import socket
 import sys
 
 
+def send_request(hostname, port, request):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        sock.connect((hostname, port))
+
+        # Send the request to the server
+        sock.sendall(request.encode())
+
+        # Receive the response from the server
+        response = sock.recv(1024).decode()
+
+    return response
